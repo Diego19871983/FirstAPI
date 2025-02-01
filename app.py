e7c825b8f31fb931f980e596e18f1e2e5b54a19f
Diego19871983 <juand.sosa@urosario.edu.co>
2025-02-01 13:02:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,23 @@ def patch_task(task_id):
     save_data()
     return jsonify(task)
 
+@app.route('/tasks/completed/true', methods=['GET'])
+def get_tasks_complet():
+    l = tasks
+    l1 = []
+    for i in l:
+        if (i['completed'] == True):
+            l1.append(i)
+    return jsonify(l1)
+
+@app.route('/tasks/categories/cat_id/tasks', methods=['GET'])
+def get_tasks_category(cat_id):
+    l = tasks
+    l1 = []
+    for i in l:
+        if (i['category_id'] == cat_id):
+            l1.append(i)
+    return jsonify(l1)
+
 if __name__ == '__main__':
     app.run(debug=True)
